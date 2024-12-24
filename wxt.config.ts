ac46f0ac4b8b45fd189ca52460a0d292ad9eea4f
azabroflovski <azabroflovski@gmail.com>
2024-12-25 04:43:31 +0500

    feat: more info in manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   extensionApi: 'chrome',
   manifest: {
+    name: 'Redmine for zoomers',
+    version: '0.0.1',
+    short_name: 'Redmine for zoomers',
+    description: 'Redmine UI sucks, solution for zoomers',
     web_accessible_resources: [
       {
         matches: ['*://*.redmine.xt-xarid.uz/*'],
