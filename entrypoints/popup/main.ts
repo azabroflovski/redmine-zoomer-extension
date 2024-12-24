317cf5c80c65ae367ca7a5660e8c319d29d53ae5
azabroflovski <azabroflovski@gmail.com>
2024-12-25 04:17:18 +0500

    fix(popup): build issue top level await not supported

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -1,11 +1,13 @@
 import './style.css';
 
 const checkbox = document.querySelector('input')
-const isEnabled = await storage.getItem('local:ui')
 
-if (isEnabled) {
-  checkbox?.click()
-}
+storage.getItem<boolean>('local:ui')
+  .then(isEnabled => {
+    if (isEnabled) {
+      checkbox?.click()
+    }
+  })
 
 checkbox?.addEventListener('input', async (event) => {
   await storage.setItem('local:ui', checkbox.checked)
